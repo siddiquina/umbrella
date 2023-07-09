631abb711c9f27fcb34c0a9cb9825fdf4515f404
siddiquina <137005373+siddiquina@users.noreply.github.com>
2023-07-09 14:26:35 +0000

    Geocoding part working

--- umbrella.rb
+++ umbrella.rb
@@ -2,13 +2,31 @@ require "http"
 
 pp "Where are you located?"
 
-# user_location = gets.chomp
-user_location = Chicago
+user_location = gets.chomp.gsub(" ", "%20")
+# user_location = "Chicago"
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
-pp maps_url
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
+
 
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
