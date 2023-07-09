e3dadfc64be7efe50a32152f82bc1668c762c909
siddiquina <137005373+siddiquina@users.noreply.github.com>
2023-07-09 11:52:41 +0000

    Color issue

--- umbrella.rb
+++ umbrella.rb
@@ -1,8 +1,18 @@
 require "http"
 
+pp "Where are you located?"
+
+# user_location = gets.chomp
+user_location = Chicago
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+pp maps_url
+
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "user_location"
 
 # Place a GET request to the URL
 raw_response = HTTP.get(pirate_weather_url)
